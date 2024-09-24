062e675eb4d16d292324e3180ec09b89726b8790
looooo <sppedflyer@gmail.com>
2024-09-24 20:03:48 +0200

    add qt6 again

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,9 @@ cmake -G "Ninja" -B build -S . ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE="Release" ^
       -D USE_QT5=ON ^
-      -D SIMAGE_USE_CPACK=OFF
+      -D SIMAGE_USE_CPACK=OFF ^
+      -D SIMAGE_LIBSNDFILE_SUPPORT=OFF ^
+      -D SIMAGE_USE_QT6=ON
 
 if errorlevel 1 exit 1
 ninja install
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,8 @@ cmake -G "Ninja" -B build -S . \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D CMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       -D CMAKE_BUILD_TYPE="Release" \
-      -D SIMAGE_USE_CPACK=OFF
+      -D SIMAGE_USE_CPACK=OFF \
+      -D SIMAGE_LIBSNDFILE_SUPPORT=OFF \
+      -D SIMAGE_USE_QT6=ON
 
 ninja install
