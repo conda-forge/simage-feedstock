2fe904dd2c6272caa09da18c1e9f245926d3f302
looooo <sppedflyer@gmail.com>
2024-09-24 19:24:19 +0200

    update to version 1.8.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,6 @@ cmake -G "Ninja" \
       -D CMAKE_PREFIX_PATH=$PREFIX \
       -D CMAKE_INSTALL_LIBDIR=$PREFIX/lib \
       -D CMAKE_BUILD_TYPE="Release" \
-      -D USE_QT5=ON \
       ..
 
 ninja install
